41447c334219760894522ee54692e5673fb7235e
Maik Schneider <m.schneider@blueways.de>
2022-10-30 18:41:47 +0100

    Squashed 'packages/deployer-extended-typo3/' content from commit d8fb25e
    git-subtree-dir: packages/deployer-extended-typo3
    git-subtree-split: d8fb25ee389a2d63c7fcdd26786c06af45acbbee

---
+++ deployer/default/set.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Deployer;
+
+set('branch_detect_to_deploy', false);
+
+set('allow_anonymous_stats', false);
+
+set('shared_files', [
+    '.env'
+]);
+
+set('writable_dirs', function () {
+    return [
+        get('web_path') . 'typo3conf',
+        get('web_path') . 'typo3temp',
+        get('web_path') . 'uploads',
+        get('web_path') . 'fileadmin'
+    ];
+});
+
+set('default_timeout', 900);
+
+set('clear_paths', [
+    '.ddev',
+    '.envrc',
+    '.git',
+    '.gitattributes',
+    '.githooks',
+    '.gitignore',
+    '.php_cs',
+    '.php-cs-fixer.php',
+    'dynamicReturnTypeMeta.json',
+    'composer.json',
+    'composer.lock',
+    'composer.phar',
+    'phpstan.neon'
+]);
+
+set('bin/typo3cms', './vendor/bin/typo3cms');
+set('local/bin/typo3cms', './vendor/bin/typo3cms');
+
+// Look https://github.com/sourcebroker/deployer-extended-media for docs
+set('media_allow_push_live', false);
+set('media_allow_copy_live', false);
+set('media_allow_link_live', false);
+set('media_allow_pull_live', false);
+set('media', function () {
+    return [
+        'filter' => [
+            '+ /' . get('web_path') . '',
+            '+ /' . get('web_path') . 'fileadmin/',
+            '- /' . get('web_path') . 'fileadmin/_processed_/*',
+            '+ /' . get('web_path') . 'fileadmin/**',
+            '+ /' . get('web_path') . 'uploads/',
+            '+ /' . get('web_path') . 'uploads/**',
+            '- *'
+        ]
+    ];
+});
+
+// Look https://github.com/sourcebroker/deployer-extended-database for docs
+set('db_allow_push_live', false);
+set('db_allow_pull_live', false);
+set('db_allow_copy_live', false);
+set('db_ddev_database_config', [
+    'host' => 'db',
+    'port' => $_ENV['DDEV_HOST_DB_PORT'] ?? null,
+    'dbname' => 'db',
+    'user' => 'db',
+    'password' => 'db',
+]);
+
+// Look https://github.com/sourcebroker/deployer-extended-database#db-dumpclean for docs
+set('db_dumpclean_keep', [
+    '*' => 5,
+    'live' => 10,
+]);
+
+
