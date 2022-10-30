d568b33b1c9f61a78b3edc9a7796ac62609e3b9c
Maik Schneider <m.schneider@blueways.de>
2022-10-30 10:45:01 +0100

    feat: install deployer v7

--- deploy.php
+++ deploy.php
@@ -1,5 +1,23 @@
 <?php
+namespace Deployer;
 
-host('deployer-v7.maik-tailor.de')
+require 'recipe/cakephp.php';
+
+// Config
+
+set('repository', 'git@github.com:maikschneider/deployer-v7-app.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+host('schnupfspruch')
+    ->set('hostname', '84.38.66.101')
+    ->set('port', '24327')
     ->set('remote_user', 'web0')
-    ->set('deploy_path', '/home/web0/vhosts/deployer-v7.maik-tailor.de/htdocs/');
+    ->set('deploy_path', '/home/web0/vhosts/deployer-v7.maik-tailor.de/htdocs');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
