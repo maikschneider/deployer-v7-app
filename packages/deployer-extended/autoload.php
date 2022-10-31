1a8401400e808bdb4b181f6f2be8b64e3c6a7c5e
Maik Schneider <m.schneider@blueways.de>
2022-10-31 15:16:45 +0100

    feat: add db:decompress command

--- packages/deployer-extended/autoload.php
+++ packages/deployer-extended/autoload.php
@@ -5,3 +5,4 @@ require __DIR__ . '/set.php';
 require __DIR__ . '/recipe/db/export.php';
 require __DIR__ . '/recipe/db/process.php';
 require __DIR__ . '/recipe/db/compress.php';
+require __DIR__ . '/recipe/db/decompress.php';
